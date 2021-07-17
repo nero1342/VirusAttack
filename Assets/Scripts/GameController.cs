1f08e6c158f6f46d57caf60d83129d049f0a0803
hdmthao <minhthaohaduc@gmail.com>
2021-07-17 13:02:26 +0700

    [ADD] game turn

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,28 +3,104 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using Assets.Scripts;
 
 public class GameController : MonoBehaviour
 {
-    public Text moneyText;
-	int moneyAmount;
-    // Start is called before the first frame update
-    void Start()
-    {
-        // PlayerPrefs.DeleteAll ();
-    }
+public static GameState gameState = GameState.Start;
+private static int turn;
+public Text moneyText;
+int moneyAmount;
+Enemy[] enemies;
+public GameObject gunObject;
+[SerializeField] string _nextLevelName;
+Gun gun;
+Vector3 StartingPosition;
 
-    // Update is called once per frame
-    void Update()
-    {
+// Start is called before the first frame update
+void Start()
+{
+        turn = 0;
+        gameState = GameState.Start;
+        enemies = FindObjectsOfType<Enemy>();
+        gun = gunObject.GetComponent<Gun>();
+        StartingPosition = Camera.main.transform.position;
+}
+
+// Update is called once per frame
+void Update()
+{
         moneyAmount = PlayerPrefs.GetInt ("MoneyAmount");
-		moneyText.text = "Money: " + moneyAmount.ToString() + "$";
-    }
+        moneyText.text = "Money: " + moneyAmount.ToString() + "$";
+        switch (gameState)
+        {
+        case GameState.Start:
+                NewTurn();
+                break;
+        case GameState.Playing:
+                if (gun.isDone()) {
+                        EndTurn();
+                        break;
+                }
+                break;
+        case GameState.Won:
+                gotoNextLevel();
+                break;
+        case GameState.Lost:
+                Debug.Log("Lost");
+                break;
+        default:
+                break;
+        }
+}
 
-    public void gotoShop()
-	{
-		PlayerPrefs.SetInt ("MoneyAmount", moneyAmount);
+public void NewTurn()
+{
+        turn += 1;
+        gameState = GameState.Playing;
+        gun.reset();
+        AnimateCameraToStartPosition();
+}
+
+public void EndTurn()
+{
+        bool isWin = IsWin();
+
+        if (isWin) {
+                gameState = GameState.Won;
+                return;
+        }
+        if (turn >= 3) {
+                gameState = GameState.Lost;
+                return;
+        }
+        gameState = GameState.Start;
+}
+
+bool IsWin() {
+        foreach (var enemy in enemies)
+        {
+                if (enemy.gameObject.activeSelf) {
+                        return false;
+                }
+        }
+        return true;;
+}
+
+private void AnimateCameraToStartPosition()
+{
+        //animate the camera to start
+        Camera.main.transform.position = StartingPosition;
+}
+
+void gotoNextLevel() {
+        SceneManager.LoadScene(_nextLevelName);
+}
+
+public void gotoShop()
+{
+        PlayerPrefs.SetInt ("MoneyAmount", moneyAmount);
         PlayerPrefs.SetString ("PreviousScene", SceneManager.GetActiveScene().name);
-		SceneManager.LoadScene ("ShopScene");
-	}
+        SceneManager.LoadScene ("ShopScene");
+}
 }
